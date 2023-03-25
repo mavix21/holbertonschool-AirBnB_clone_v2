37a67791b7d8f69b1bc0110a273590ceffb31b79
mavix21 <marcevizcarra2109@hotmail.com>
2023-03-25 18:35:04 -0500

    Feat: Storage type managment

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from .engine.file_storage import FileStorage
+import os
 
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
-storage = FileStorage()
-storage.reload()
+if storage_type == 'db':
+    from .engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from .engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
