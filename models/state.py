b676d6582ea47c68625b0a92dac2c4c8ae0ee0d1
mavix21 <marcevizcarra2109@hotmail.com>
2023-03-25 18:40:28 -0500

    Set attributes for class to work with sqlalchemy orm

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from .base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from .city import City
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+
+    if models.storage_type == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
+
+# City.state = relationship("State", back_populates="cities")
