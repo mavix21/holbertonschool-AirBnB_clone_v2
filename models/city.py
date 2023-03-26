60ea3160b3e4354b10d6bdba4403994fbd0576f0
mavix21 <marcevizcarra2109@hotmail.com>
2023-03-26 00:41:58 -0500

    Remove import models to prevent circular import

--- models/city.py
+++ models/city.py
@@ -2,10 +2,15 @@
 """ City Module for HBNB project """
 from .base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+import os
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    if os.environ.get('HBNB_TYPE_STORAGE'):
+        __tablename__ = "cities"
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    else:
+        name = ""
+        state_id = ""
