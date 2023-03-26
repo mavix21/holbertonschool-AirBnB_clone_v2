c9d5334b1ccbc1e3deb6145e9829dd489dca21c3
mavix21 <marcevizcarra2109@hotmail.com>
2023-03-26 02:20:55 -0500

    Add missing column mappings

--- models/place.py
+++ models/place.py
@@ -14,6 +14,8 @@ class Place(BaseModel, Base):
         name = Column(String(128), nullable=False)
         description = Column(String(1024))
         number_rooms = Column(Integer, nullable=False, default=0)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float)
         longitude = Column(Float)
