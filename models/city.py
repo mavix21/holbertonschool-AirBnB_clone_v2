ab1252004327838ca908a2dd39bd54bbc141b453
mavix21 <marcevizcarra2109@hotmail.com>
2023-03-26 09:32:57 -0500

    Fix conditional

--- models/city.py
+++ models/city.py
@@ -9,7 +9,7 @@ import os
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    if os.environ.get('HBNB_TYPE_STORAGE'):
+    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = "cities"
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
