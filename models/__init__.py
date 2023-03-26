1e275fa0980b77ba33dac3238bb52cb402b11da6
mavix21 <marcevizcarra2109@hotmail.com>
2023-03-26 00:39:17 -0500

    Refactorization

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 import os
+from .engine.db_storage import DBStorage
+from .engine.file_storage import FileStorage
 
 storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
 if storage_type == 'db':
-    from .engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
 else:
-    from .engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+
+storage.reload()
