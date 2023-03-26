13431457c6b5166e66a1417f674bdadcbe23c13d
mavix21 <marcevizcarra2109@hotmail.com>
2023-03-26 11:37:57 -0500

    Add getters and setters for amenities

--- models/place.py
+++ models/place.py
@@ -2,9 +2,30 @@
 """ Place Module for HBNB project """
 from .base_model import BaseModel, Base
 from .review import Review
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from .amenity import Amenity
+from sqlalchemy import Column, Table, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import relationship
 import os
+
+if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+    place_amenity = Table(
+            'place_amenity',
+            Base.metadata,
+            Column(
+                'place_id',
+                String(60),
+                ForeignKey('places.id'),
+                nullable=False,
+                primary_key=True
+                ),
+            Column(
+                'amenity_id',
+                String(60),
+                ForeignKey('amenities.id'),
+                nullable=False,
+                primary_key=True
+                )
+            )
 
 
 class Place(BaseModel, Base):
@@ -22,6 +43,10 @@ class Place(BaseModel, Base):
         latitude = Column(Float)
         longitude = Column(Float)
         reviews = relationship("Review", backref="place", cascade="all")
+        amenities = relationship("Amenity",
+                                 secondary="place_amenity",
+                                 backref="place_amenity",
+                                 viewonly=False)
     else:
         city_id = ""
         user_id = ""
@@ -37,6 +62,21 @@ class Place(BaseModel, Base):
 
         @property
         def reviews(self):
+            """The reviews property."""
             import models
             return [review for review in models.storage.all(Review).values()
                     if review.place_id == self.id]
+
+        @property
+        def amenities(self):
+            """The amenities property."""
+            import models
+            return [amenity for amenity in models.storage.all(Amenity).values()
+                    if amenity.id in self.amenity_ids]
+
+        @amenities.setter
+        def amenities(self, value):
+            if type(value) is not Amenity:
+                return
+
+            self.amenity_ids.append(value.id)
