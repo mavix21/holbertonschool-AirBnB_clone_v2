a4378cafb76fa6631a80f56eaeed5252e4af2bdd
mavix21 <marcevizcarra2109@hotmail.com>
2023-04-06 19:30:48 -0500

    Script that starts a Flask web application which listens on 0.0.0.0, port 5000, with two routes

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This module starts a Flask Web application that listens on 0.0.0.0
+    (all network interfaces) and port 5000
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_webapp():
+    """Returns the message we want to display in the user's browser"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """Returns the message we want to display in the user's browser"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
