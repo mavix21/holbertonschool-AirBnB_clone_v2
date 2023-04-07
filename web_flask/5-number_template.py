ae71249b94ae64e146bf998e50304e70d5a545a3
mavix21 <marcevizcarra2109@hotmail.com>
2023-04-07 12:48:28 -0500

    Escape endpoint

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -46,8 +46,8 @@ def only_if_number(n):
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
-def render_template_number(n):
-    return render_template("5-number.html", n=n)
+def template_number(n):
+    return render_template("5-number.html", num=escape(n))
 
 
 if __name__ == "__main__":
